72d71c2d3d60e9fb4640d512ebdb2a7cc09afd75
ahmed walid <ahmed.walid21323@gmail.com>
2026-06-06 16:20:51 +0300

    Update connection string for hosted SQL Server database

---
+++ src/ECommerce.Persistence/Migrations/20260606130140_InitialCreate1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLoginAt",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
